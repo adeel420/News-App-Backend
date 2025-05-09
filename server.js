f51b4744aa0181fe49430dad362ed171502e85ff
Your Name <your_email@example.com>
2025-05-09 22:29:50 +0500

    add backend

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const PORT = process.env.PORT || 8080
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 // package
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // files
-const db = require('./db')
-const passport = require('./middleware/auth')
-app.use(passport.initialize())
-const authMiddleware = passport.authenticate('local', {session: false})
-const userRoutes = require('./routes/userRoutes')
-const categoryRoutes = require('./routes/categoryRoutes')
-const newsRoutes = require('./routes/NewsRoutes') 
-const commentRoutes = require('./routes/commentRoutes') 
-const emailConfig = require('./middleware/emailConfig')
+const db = require("./db");
+const passport = require("./middleware/auth");
+app.use(passport.initialize());
+const authMiddleware = passport.authenticate("local", { session: false });
+const userRoutes = require("./routes/userRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const newsRoutes = require("./routes/NewsRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const emailConfig = require("./middleware/emailConfig");
 
-//routes 
-app.use('/user', userRoutes)
-app.use('/category', categoryRoutes)
-app.use('/news', newsRoutes)
-app.use('/comment', commentRoutes)
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+//routes
+app.use("/user", userRoutes);
+app.use("/category", categoryRoutes);
+app.use("/news", newsRoutes);
+app.use("/comment", commentRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Listening the port ${PORT}`)
-})
+  console.log(`Listening the port ${PORT}`);
+});
